ab7995b89eedb790e08da38302d02669326aaf19
Anvesh Dangeti <anvesh.d2020@gmail.com>
2025-05-22 23:38:41 +0530

    Added Monaco editor for live coiding

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
-import { useEffect, useState } from 'react';
+// client/src/App.js
+import { useState } from 'react';
+import Editor from '@monaco-editor/react';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [code, setCode] = useState('// Start coding here...');
 
-  useEffect(() => {
-    fetch('http://localhost:5000/')
-      .then(res => res.text())
-      .then(setMessage);
-  }, []);
+  const handleCodeChange = (value) => {
+    setCode(value);
+  };
 
-  return <h1>{message}</h1>;
+  const handleRun = () => {
+    alert('Code submitted:\n\n' + code);
+  };
+
+  return (
+    <div className="App">
+      <h1>Live Coding Interview Platform</h1>
+      <Editor
+        height="500px"
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        onChange={handleCodeChange}
+      />
+      <button onClick={handleRun}>Run Code</button>
+    </div>
+  );
 }
 
 export default App;
